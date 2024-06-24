74c8790bd3f20a4adfb798dcc85cdda232d2ca0c
Acedwa227 <adam.edwards227@yahoo.com>
2024-06-23 20:23:40 -0500

    Add files via upload

---
+++ Wk5 Files html hw.py
@@ -0,0 +1,23 @@
+def main():
+    name = input('Enter your name: ')
+    description = input ('Describe yourself: ')
+    outfile = open('mypage.html', 'w')
+
+    outfile.write('<html>')
+
+    outfile.write('<body>')
+    outfile.write('<center>')
+    outfile.write('<h1>')
+    outfile.write(name)
+    outfile.write('</h1>')
+    outfile.write('</center>')
+    outfile.write('<hr />')
+    outfile.write(description)
+    outfile.write('<hr />')
+    outfile.write('</body>')
+    outfile.write('</html>')
+
+    outfile.close
+
+main()
+                
